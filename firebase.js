535a29bb44f495498277910ba7d6dcf60f5a44eb
Fabio <xk73mh31@gmail.com>
2026-05-19 23:18:57 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -11,19 +11,12 @@ from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 /* CONFIG FIREBASE */
 
 const firebaseConfig = {
-
   apiKey: "AIzaSyAU9shDRZKZ5jBeHCm-e59aMIvDIoNBO8E",
-
   authDomain: "database-ordini-kitho.firebaseapp.com",
-
   projectId: "database-ordini-kitho",
-
   storageBucket: "database-ordini-kitho.firebasestorage.app",
-
   messagingSenderId: "711821639626",
-
   appId: "1:711821639626:web:f98d52f647d90282fed933",
-
   measurementId: "G-DL6NJX5J7T"
 };
 
