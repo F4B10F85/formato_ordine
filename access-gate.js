7db98a8e6d3e9c0065ed11cf6814d63ed64b0f21
Fabio <xk73mh31@gmail.com>
2026-05-24 23:37:48 +0200

    Update access-gate.js

--- access-gate.js
+++ access-gate.js
@@ -1,1 +1,46 @@
+const overlay = document.getElementById("accessOverlay");
 
+const privateBtn =
+  document.getElementById("privateAccessBtn");
+
+const shopBtn =
+  document.getElementById("shopAccessBtn");
+
+const shopLoginBox =
+  document.getElementById("shopLoginBox");
+
+const loginBtn =
+  document.getElementById("shopLoginBtn");
+
+const loginError =
+  document.getElementById("loginError");
+
+privateBtn.addEventListener("click", () => {
+
+  overlay.style.display = "none";
+
+});
+
+shopBtn.addEventListener("click", () => {
+  shopLoginBox.classList.remove("hidden");
+});
+
+loginBtn.addEventListener("click", () => {
+  const username =
+    document.getElementById("shopUsername").value;
+  const password =
+    document.getElementById("shopPassword").value;
+
+  if (
+    username === "negozio" &&
+    password === "1234"
+  ) {
+
+    overlay.style.display = "none";
+    localStorage.setItem("shopUser", "true");
+  } else {
+    loginError.style.display = "block";
+
+  }
+
+});
