20e91d659c55e825d92ab46e924ca37dbc482fe1
Fabio <xk73mh31@gmail.com>
2026-05-24 17:52:29 +0200

    Update access.js

--- access.js
+++ access.js
@@ -1,1 +1,24 @@
+const accessOverlay =
+  document.getElementById("accessOverlay");
 
+const privateAccessBtn =
+  document.getElementById("privateAccessBtn");
+
+const shopAccessBtn =
+  document.getElementById("shopAccessBtn");
+
+
+privateAccessBtn.addEventListener("click", () => {
+
+  accessOverlay.style.display = "none";
+
+});
+
+
+shopAccessBtn.addEventListener("click", () => {
+
+  alert(
+    "Area negozi in arrivo"
+  );
+
+});
